c24f2d935fef7ca09d23c0d48dcb6791e88ad9d5
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-11 18:30:26 -0500

    adding webpack development config

---
+++ webpack/webpack.development.config.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.config.js')
+
+module.exports = () => ({
+	...merge(baseConfig(), {
+		mode: 'development',
+		module: {
+			rules: [{
+				test: /\.s?css$/,
+				loader: ['style-loader', 'css-loader', 'sass-loader']
+			}]
+		},
+		devServer: {
+			host: '0.0.0.0',
+			open: false,
+			historyApiFallback: true
+		}
+	})
+});
