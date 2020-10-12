c7cc4109fa56f9fe66223921a731de390191c10b
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-11 21:52:10 -0500

    adding ts loader

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,24 +1,34 @@
 const HtmlPlugin = require('html-webpack-plugin');
-const { resolve } = require('path')
+const { resolve } = require('path');
 
 module.exports = () => {
-	const rootPath = resolve(__dirname, '..');
-	const srcPath = resolve(rootPath, 'src');
-	return {
-		entry: resolve(srcPath, 'app', 'index.tsx'),
-		output: {
-			filename: 'app-[hash:6].js',
-			path: resolve(rootPath, 'dist'),
-			publicPath: '/'
-		},
-		resolve: {
-			extensions: ['.ts', '.tsx', '.js', '.jsx'],
-		},
-		plugins: [
-			new HtmlPlugin({
-				template: resolve(srcPath, 'public', 'index.html'),
-				filename: 'index.html'
-			})
-		]
-	}
-}
+  const rootPath = resolve(__dirname, '..');
+  const srcPath = resolve(rootPath, 'src');
+  return {
+    entry: resolve(srcPath, 'app', 'index.tsx'),
+    output: {
+      filename: 'app-[hash:6].js',
+      path: resolve(rootPath, 'dist'),
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: resolve(srcPath, 'public', 'index.html'),
+        filename: 'index.html',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: {
+            loader: 'awesome-typescript-loader',
+          },
+        },
+      ],
+    },
+  };
+};
