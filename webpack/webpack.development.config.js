cd197f6ab7d4ae29455bab2eb052d2457fd7df5f
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-11 21:33:41 -0500

    adding ts, eslint, prettier and editor config

--- webpack/webpack.development.config.js
+++ webpack/webpack.development.config.js
@@ -1,19 +1,21 @@
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base.config.js')
+const baseConfig = require('./webpack.base.config.js');
 
 module.exports = () => ({
-	...merge(baseConfig(), {
-		mode: 'development',
-		module: {
-			rules: [{
-				test: /\.s?css$/,
-				loader: ['style-loader', 'css-loader', 'sass-loader']
-			}]
-		},
-		devServer: {
-			host: '0.0.0.0',
-			open: false,
-			historyApiFallback: true
-		}
-	})
+  ...merge(baseConfig(), {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          loader: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    devServer: {
+      host: '0.0.0.0',
+      open: false,
+      historyApiFallback: true,
+    },
+  }),
 });
