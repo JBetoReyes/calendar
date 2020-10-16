104f0fab670b25cc142ccfed34f0d07dea05226b
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-15 22:27:16 -0500

    adding fort awesome

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = (): JSX.Element => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4">
       <span className="navbar-brand">Beto R.</span>
       <button type="button" className="btn btn-outline-danger">
-        <span>exit</span>
+        <FontAwesomeIcon icon={faSignInAlt} /> <span>exit</span>
       </button>
     </div>
   );
