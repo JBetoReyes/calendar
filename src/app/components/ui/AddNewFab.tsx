a0d4b7934e11797b9dee14f242af8407d25c296e
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-29 17:52:00 -0600

    adding handleButtonClick

--- src/app/components/ui/AddNewFab.tsx
+++ src/app/components/ui/AddNewFab.tsx
@@ -1,14 +1,33 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { connect } from 'react-redux';
+import { IStoreState } from 'src/app/store/storeModel';
+import { AppClickEvent } from 'src/typings/htmlEvents';
+import { openModal } from '../../reducers/uiActions';
 import './AddNewFab.scss';
 
-const AddNewFab = () => {
+const mapDispatchToProps = {
+  openModal,
+};
+type DispatchPropsType = typeof mapDispatchToProps;
+type PropsType = DispatchPropsType;
+const AddNewFab = ({ openModal: dispatchOpenModal }: PropsType) => {
+  const handleButtonClick = (e: AppClickEvent) => {
+    dispatchOpenModal();
+  };
   return (
-    <button type="button" className="btn btn-primary fab">
+    <button
+      type="button"
+      className="btn btn-primary fab"
+      onClick={handleButtonClick}
+    >
       <FontAwesomeIcon icon={faPlus} size="2x" />
     </button>
   );
 };
 
-export default AddNewFab;
+export default connect<null, DispatchPropsType, any, IStoreState>(
+  null,
+  mapDispatchToProps,
+)(AddNewFab);
