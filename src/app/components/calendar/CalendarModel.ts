4d510c8660f5726e57e486f8002609dafc5d01d8
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-29 19:33:01 -0600

    adding properties to the app calendar event

--- src/app/components/calendar/CalendarModel.ts
+++ src/app/components/calendar/CalendarModel.ts
@@ -2,5 +2,7 @@ import { Event as CalendarEvent } from 'react-big-calendar';
 import { IAppUser } from '../auth/UserModel';
 
 export interface IAppCalendarEvent extends CalendarEvent {
+  id?: string;
+  notes?: string;
   user?: IAppUser;
 }
