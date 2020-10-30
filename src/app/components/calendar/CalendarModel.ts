c4f198c22821a14cd8504a7173f8889556915746
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-30 12:21:41 -0600

    making id required

--- src/app/components/calendar/CalendarModel.ts
+++ src/app/components/calendar/CalendarModel.ts
@@ -2,7 +2,7 @@ import { Event as CalendarEvent } from 'react-big-calendar';
 import { IAppUser } from '../auth/UserModel';
 
 export interface IAppCalendarEvent extends CalendarEvent {
-  id?: string;
+  id: string;
   notes?: string;
   user?: IAppUser;
 }
