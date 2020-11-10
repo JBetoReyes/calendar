70a981fda393eacf4f3bb5d6470a696a3efebe35
beto <josereyes55@hotmail.com>
2020-11-09 18:21:13 -0600

    trying get this a little bit prettier

--- src/app/helpers/fetch.tsx
+++ src/app/helpers/fetch.tsx
@@ -1,29 +1,41 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type MethodCallType = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 const baseUrl = process.env.API_APP_URL;
 
-export const appFetch = (endpoint: string, method: MethodCallType = 'GET', data?: any, headers: Record<string, string> = {}) => {
+export const appFetch = (
+  endpoint: string,
+  method: MethodCallType = 'GET',
+  data?: any,
+  headers: Record<string, string> = {},
+) => {
   const url = `${baseUrl}/${endpoint}`;
   if (method === 'GET') {
-    return fetch(url,{
+    return fetch(url, {
       method,
       headers,
     });
-  } else {
-    return fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-        ...headers,
-      },
-      body: JSON.stringify(data ? data : ''),
-    });
   }
-}
+  return fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify(data || ''),
+  });
+};
 
-export const appFetchWithToken = (endpoint: string, method: MethodCallType = 'GET', data?: any, headers: Record<string, string> = {}) => {
+export const appFetchWithToken = (
+  endpoint: string,
+  method: MethodCallType = 'GET',
+  data?: any,
+  headers: Record<string, string> = {},
+): Promise<Response> | undefined => {
   const token = localStorage.getItem('token');
   if (token) {
-    return appFetch(endpoint, method, data, { ...headers, 'x-token': token});
+    return appFetch(endpoint, method, data, {...headers, 'x-token': token});
   }
-}
+};
