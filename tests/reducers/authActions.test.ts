f2fe32bd0a6c9a88d5ca2f2e42ff320b77132b8a
beto <josereyes55@hotmail.com>
2020-11-10 21:39:10 -0600

    adding fail scenario

--- tests/reducers/authActions.test.ts
+++ tests/reducers/authActions.test.ts
@@ -2,10 +2,14 @@ import {FetchMock} from 'jest-fetch-mock/types';
 import {AnyAction} from 'redux';
 import configureStore, {MockStoreEnhanced} from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import {login, startLogin} from '../../src/app/reducers/authActions';
 
 Storage.prototype.setItem = jest.fn();
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn(),
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -47,5 +51,23 @@ describe('authActions', () => {
       expect.any(String),
     );
     expect(actions[0]).toEqual(login({uid: loginPayload.id, ...loginPayload}));
+  });
+
+  test('startLogin with a fail scenario', async () => {
+    (fetch as FetchMock).mockResponseOnce(
+      JSON.stringify({
+        ok: false,
+        msg: 'Something went wrong',
+      }),
+    );
+    await store.dispatch(
+      (startLogin('test@email.com', '123456') as unknown) as AnyAction,
+    );
+    expect(store.getActions()).toEqual([]);
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'Something went wrong',
+      'error',
+    );
   });
 });
