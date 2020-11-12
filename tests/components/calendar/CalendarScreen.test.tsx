ab547e71f3748328a1eb24a1030f41a9dcdcfbfa
beto <josereyes55@hotmail.com>
2020-11-11 22:04:53 -0600

    adding more tests to the calendar screen

--- tests/components/calendar/CalendarScreen.test.tsx
+++ tests/components/calendar/CalendarScreen.test.tsx
@@ -1,17 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {mount, ReactWrapper} from 'enzyme';
 import React from 'react';
+import {act} from 'react-dom/test-utils';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import CalendarScreen from '../../../src/app/components/calendar/CalendarScreen';
 import {openModal} from '../../../src/app/reducers/uiActions';
+import {setActiveEvent} from '../../../src/app/reducers/calendarActions';
 
 jest.mock('../../../src/app/reducers/uiActions', () => {
   return {
     openModal: jest.fn(),
   };
 });
+jest.mock('../../../src/app/reducers/calendarActions', () => {
+  return {
+    setActiveEvent: jest.fn(),
+    startGetEvents: jest.fn(),
+  };
+});
+Storage.prototype.setItem = jest.fn();
 
 const middlewares = [thunk];
 const mockStoreProvider = configureStore(middlewares);
@@ -34,5 +43,15 @@ describe('CalendarScreen', () => {
   test('Should dispatch open modal', () => {
     calendar.prop('onDoubleClickEvent')();
     expect(openModal).toHaveBeenCalledWith();
+  });
+  test('Should dispatch set active event', () => {
+    calendar.prop('onSelectEvent')({view: 'week'});
+    expect(setActiveEvent).toHaveBeenCalledWith({view: 'week'});
+  });
+  test('Should set the calendar view', () => {
+    act(() => {
+      calendar.prop('onView')('week');
+      expect(localStorage.setItem).toHaveBeenCalledWith('view', 'week');
+    });
   });
 });
