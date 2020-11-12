0348aa8d5d4fa57144323d24f549d61f8a2edbba
beto <josereyes55@hotmail.com>
2020-11-11 21:41:09 -0600

    adding an open modal scenario to the calendar screen tests

--- tests/components/calendar/CalendarScreen.test.tsx
+++ tests/components/calendar/CalendarScreen.test.tsx
@@ -1,9 +1,17 @@
-import {mount} from 'enzyme';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {mount, ReactWrapper} from 'enzyme';
 import React from 'react';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import CalendarScreen from '../../../src/app/components/calendar/CalendarScreen';
+import {openModal} from '../../../src/app/reducers/uiActions';
+
+jest.mock('../../../src/app/reducers/uiActions', () => {
+  return {
+    openModal: jest.fn(),
+  };
+});
 
 const middlewares = [thunk];
 const mockStoreProvider = configureStore(middlewares);
@@ -18,8 +26,13 @@ const wrapper = mount(
     <CalendarScreen />
   </Provider>,
 );
+const calendar = wrapper.find('Calendar') as ReactWrapper<any, any>;
 describe('CalendarScreen', () => {
   test('Should render the calendar', () => {
     expect(wrapper.find('.rbc-calendar').length).toBe(1);
+  });
+  test('Should dispatch open modal', () => {
+    calendar.prop('onDoubleClickEvent')();
+    expect(openModal).toHaveBeenCalledWith();
   });
 });
