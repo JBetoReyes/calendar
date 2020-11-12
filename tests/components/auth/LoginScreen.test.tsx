ba53216477b640e5f35991c4f66ae88cced40617
beto <josereyes55@hotmail.com>
2020-11-11 20:23:27 -0600

    adding a test for the login screen

--- tests/components/auth/LoginScreen.test.tsx
+++ tests/components/auth/LoginScreen.test.tsx
@@ -3,16 +3,22 @@ import thunk from 'redux-thunk';
 import {mount, ReactWrapper} from 'enzyme';
 import React from 'react';
 import {Provider} from 'react-redux';
+import Swal from 'sweetalert2';
 
 // eslint-disable-next-line import/no-named-as-default
 import LoginScreen from '../../../src/app/components/auth/LoginScreen';
-import {startLogin} from '../../../src/app/reducers/authActions';
+import {startLogin, startRegister} from '../../../src/app/reducers/authActions';
 
 jest.mock('../../../src/app/reducers/authActions', () => {
   return {
     startLogin: jest.fn(),
+    startRegister: jest.fn(),
   };
 });
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn(),
+}));
 
 const middleware = [thunk];
 const mockStoreProvider = configureStore(middleware);
@@ -43,5 +49,24 @@ describe('Login screen', () => {
       .simulate('change', {target: {name: 'lPassword', value: 'password'}});
     wrapper.find('.login-form-1 form').simulate('submit');
     expect(startLogin).toHaveBeenCalledWith('test@email.com', 'password');
+  });
+  test('Should not dispatch start register', () => {
+    wrapper.find('input[name="rPassword"]').simulate('change', {
+      name: 'rPassword',
+      value: '12345678',
+    });
+    wrapper.find('input[name="rConfirmPassword"]').simulate('change', {
+      target: {
+        name: 'rConfirmPassword',
+        value: '123456789',
+      },
+    });
+    wrapper.find('.login-form-2 form').simulate('submit');
+    expect(startRegister).toHaveBeenCalledTimes(0);
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'Password should match.',
+      'error',
+    );
   });
 });
