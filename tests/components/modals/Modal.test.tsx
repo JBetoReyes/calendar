d1b04f9f05d954e8bdbdc38264f76256c0d4aedc
beto <josereyes55@hotmail.com>
2020-11-12 22:31:34 -0600

    adding tests scenarios, epic last video

--- tests/components/modals/Modal.test.tsx
+++ tests/components/modals/Modal.test.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 import moment from 'moment';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { Provider } from 'react-redux';
+import {act} from 'react-dom/test-utils';
+import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import Modal from '../../../src/app/components/modals/Modal';
-import { startUpdateEvent } from '../../../src/app/reducers/calendarActions';
+import {
+  startAddActiveEvent,
+  startUpdateEvent,
+} from '../../../src/app/reducers/calendarActions';
 
 jest.mock('../../../src/app/reducers/calendarActions', () => {
   return {
@@ -16,7 +20,11 @@ jest.mock('../../../src/app/reducers/calendarActions', () => {
     startAddActiveEvent: jest.fn(),
   };
 });
-
+jest.mock('sweetalert2', () => {
+  return {
+    fire: jest.fn(),
+  };
+});
 const middlewares = [thunk];
 const mockStoreProvider = configureStore(middlewares);
 const now = moment().minutes(0).seconds(0).add(1, 'hours');
@@ -53,7 +61,7 @@ describe('Modal', () => {
         <Modal />
       </Provider>,
     );
-    wrapper.find('form').prop('onSubmit')({ preventDefault() { } } as any);
+    wrapper.find('form').prop('onSubmit')({preventDefault() {}} as any);
     act(() => {
       wrapper.find('input[name="title"]').prop('onChange')({
         target: {
@@ -65,7 +73,7 @@ describe('Modal', () => {
     expect(startUpdateEvent).toHaveBeenCalled();
   });
   test('Should set the title field as invalis', () => {
-    const store = mockStoreProvider({
+    const testStore = mockStoreProvider({
       calendar: {
         events: [],
         activeEvent: null,
@@ -74,16 +82,75 @@ describe('Modal', () => {
         modalOpen: true,
       },
     });
-    store.dispatch = jest.fn();
+    testStore.dispatch = jest.fn();
     const wrapper = mount(
       <Provider store={store}>
         <Modal />
       </Provider>,
     );
     act(() => {
-      wrapper.find('form').prop('onSubmit')({ preventDefault() { } } as any);
+      wrapper.find('form').prop('onSubmit')({preventDefault() {}} as any);
       wrapper.mount();
     });
     expect(wrapper.find('input[name="title"]').hasClass('is-invalid'));
+  });
+  test('Should dispatch add active event', () => {
+    const testStore = mockStoreProvider({
+      calendar: {
+        events: [],
+        activeEvent: null,
+      },
+      ui: {
+        modalOpen: true,
+      },
+    });
+    testStore.dispatch = jest.fn();
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Modal />
+      </Provider>,
+    );
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        name: 'title',
+        value: 'title',
+      },
+    });
+    act(() => {
+      wrapper.find('form').simulate('submit');
+    });
+    expect(startAddActiveEvent).toHaveBeenCalledWith({
+      end: expect.anything(),
+      notes: '',
+      start: expect.anything(),
+      title: 'title',
+    });
+  });
+  test('Should validate dates before submiting', () => {
+    const testStore = mockStoreProvider({
+      calendar: {
+        events: [],
+        activeEvent: null,
+      },
+      ui: {
+        modalOpen: true,
+      },
+    });
+    testStore.dispatch = jest.fn();
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Modal />
+      </Provider>,
+    );
+    const nowDate = moment().subtract('1 hour').toDate();
+    act(() => {
+      wrapper.find('DateTimePicker').at(1).prop('onChange')(nowDate as any);
+      wrapper.mount();
+    });
+    wrapper.find('form').simulate('submit');
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'The end date cannot be before the start date',
+    );
   });
 });
